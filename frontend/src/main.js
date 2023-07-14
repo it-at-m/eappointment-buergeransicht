8b9ed4ccd580b18bed1f9e88623032e405c86312
Thomas Fink <thomas.fink@muenchen.de>
2023-07-14 17:20:21 +0200

    restructure vuetify to main.js set primary, accent and secondary colors. Bug

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,33 +1,39 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import 'vuetify/dist/vuetify.min.css';
+import App from './App.vue';
+import store from '@/store/';
+import translations from '@/translations/';
+import VueI18n from 'vue-i18n';
 
-import Vuetify from "vuetify";
-import "vuetify/dist/vuetify.min.css";
-import App from "./App.vue";
-import store from '@/store/'
-import translations from '@/translations/'
-import VueI18n from "vue-i18n";
-
-Vue.use(VueI18n)
-Vue.use(Vuetify)
+Vue.use(VueI18n);
+Vue.use(Vuetify);
 Vue.config.productionTip = false;
+
 const i18n = new VueI18n({
   locale: store.state.locale,
   messages: translations,
-})
+});
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: '#2c3e50', // Set your primary color here
+        secondary: '#1b98d5', // Set your secondary color here
+        accent: '#32a852',
+      },
+    },
+  },
+  icons: {
+    iconfont: 'mdiSvg',
+  },
+});
 
 new Vue({
-    el: "#app",
-    store,
-    i18n: i18n,
-    render: h => h(App),
-    vuetify: new Vuetify({
-        icons: {
-          iconfont: 'mdiSvg'
-        },
-        theme: {
-          defaultTheme: 'light'
-        }
-      }),
-  });
-
-
+  el: '#app',
+  store,
+  i18n,
+  vuetify,
+  render: h => h(App),
+});
