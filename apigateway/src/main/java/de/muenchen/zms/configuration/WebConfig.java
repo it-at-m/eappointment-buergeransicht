90ae84ecdd14499b99243f33d503438dc0894e02
Thomas Fink <thomas.fink@muenchen.de>
2023-07-21 10:26:36 +0200

    Add webconfig for cors headers

---
+++ apigateway/src/main/java/de/muenchen/zms/configuration/WebConfig.java
@@ -0,0 +1,28 @@
+package de.muenchen.zms.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/buergeransicht/js/**")
+                .addResourceLocations("classpath:/public/buergeransicht/js/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver() {
+                    @Override
+                    protected Resource getResource(String resourcePath, Resource location) throws IOException {
+                        Resource resource = location.createRelative(resourcePath);
+                        return resource.exists() && resource.isReadable() ? resource : null;
+                    }
+                });
+    }
+}
