db7771c7552607f4b3b6d4b05ee752f2d4efae16
Thomas Fink <thomas.fink@muenchen.de>
2023-08-02 11:47:33 +0200

    Chnage back new email name

--- frontend/src/components/CustomerInfo.vue
+++ frontend/src/components/CustomerInfo.vue
@@ -110,28 +110,19 @@ export default {
       this.$emit('changed')
     },
     saveCustomer() {
-        this.$v.$touch()
+      this.$v.$touch()
 
-        if (this.emailErrors.length || this.nameErrors.length || this.dataProtectionErrors.length) {
-          return
-        }
-
-        let savedCustomer = this.$store.state.data.customer;
-
-        if (this.isPreselectedAppointment) {
-          this.customer.name = savedCustomer.name;
-          this.customer.email = savedCustomer.email;
-        }
-
-        this.$store.dispatch('updateAppointmentData', {
-          ...this.$store.state.data.appointment,
-          client: this.customer
-        })
-        
-        this.$emit('next')
-        this.$v.$reset()
+      if (this.emailErrors.length || this.nameErrors.length || this.dataProtectionErrors.length) {
+        return
       }
 
+      this.$store.dispatch('updateAppointmentData', {
+        ...this.$store.state.data.appointment,
+        ...{ client: this.customer }
+      })
+      this.$emit('next')
+      this.$v.$reset()
+    }
   },
   mounted() {
     this.customer = this.$store.state.data.customer
