3dd9627dcec40fb713f9838e47034203176c260f
Thomas Fink <thomas.fink@muenchen.de>
2023-10-19 12:59:45 +0200

    Customtextfield display on rebooking

--- frontend/src/store/modules/formData/mutations.js
+++ frontend/src/store/modules/formData/mutations.js
@@ -67,7 +67,7 @@ export default {
     },
     setAppointment (state, appointment) {
         state.appointment = appointment
-        state.appointment.scope.customTextfieldLabel = appointment ? appointment.scope.customTextfieldLabel: null
+        //state.appointment.scope.customTextfieldLabel = appointment ? appointment.scope.customTextfieldLabel: null
     },
     setCustomerData (state, customer) {
         state.customer.name = customer ? customer.name : null
