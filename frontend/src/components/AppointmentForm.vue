c87d90212d847f26b8b310a0147cf8b085dc573c
Thomas Fink <thomas.fink@muenchen.de>
2023-10-30 13:45:44 +0100

    Fonts to folder

--- frontend/src/components/AppointmentForm.vue
+++ frontend/src/components/AppointmentForm.vue
@@ -364,6 +364,7 @@ export default {
 
 <style scoped>
 #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
