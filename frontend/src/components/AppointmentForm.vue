2e9fff351934c6b627ac0ba68b4971a9dd531af5
Thomas Fink <thomas.fink@muenchen.de>
2023-10-31 14:18:22 +0100

    Add fonts and color to text

--- frontend/src/components/AppointmentForm.vue
+++ frontend/src/components/AppointmentForm.vue
@@ -364,10 +364,10 @@ export default {
 
 <style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Open Sans, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #3a5368;
 }
 
 .theme--light.v-application{
