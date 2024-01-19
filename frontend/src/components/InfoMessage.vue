68c057b821ed42fe0580afbe383022165ff69cbd
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2024-01-19 15:20:03 +0100

    feat(ZMS-1772): create info box

---
+++ frontend/src/components/InfoMessage.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="m-component m-component-callout m-component-callout--fullwidth">
+    <div>
+      <div class="m-component__grid">
+        <div class="m-component__column">
+          <div :class="['m-callout', 'm-callout--' + type ]">
+            <div class="m-callout__inner">
+              <div class="m-callout__icon">
+                <svg aria-hidden="true" class="icon">
+                  <use v-if="type === 'success'" xlink:href="#icon-check"></use>
+                  <use v-if="type === 'warning'" xlink:href="#icon-check"></use>
+                  <use v-if="type === 'default'" xlink:href="#icon-information"></use>
+                </svg>
+                <span class="visually-hidden"></span>
+              </div>
+              <div class="m-callout__body">
+                <div class="m-callout__body__inner">
+                  <h2 class="m-callout__headline appointment-confirmation">{{ title }}</h2>
+                  <div class="m-callout__content appointment-confirmation-notice">
+                    <p>{{ text }}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InfoMessage',
+  props: {
+    type: {
+      default: 'default',
+      type: String
+    },
+    title: {
+      default: '',
+      type: String
+    },
+    text: {
+      default: '',
+      type: String
+    }
+  }
+}
+</script>
+<style scoped>
+</style>
