3cf17f8c5b1d9395bd886a9f4d62d094a2f6a488
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2024-02-02 13:56:46 +0100

    feat(ZMS-815): refactoring

--- frontend/src/components/TheCalendar.vue
+++ frontend/src/components/TheCalendar.vue
@@ -12,8 +12,8 @@
               v-model="activeProviderTab"
           >
             <v-tab
-              v-for="provider in $store.state.data.service.providers"
-              v-if="shouldShowProvider(provider)" :key="provider.index"
+              v-for="provider in filteredProviders"
+              :key="provider.index"
               @change="showForProvider(provider)"
             >
               {{ provider.name }}
@@ -24,8 +24,7 @@
               v-model="activeProviderTab"
           >
             <v-tab-item
-                v-for="provider in $store.state.data.service.providers"
-                v-if="shouldShowProvider(provider)"
+                v-for="provider in filteredProviders"
                 :key="provider.index">
             </v-tab-item>
           </v-tab-items>
@@ -140,6 +139,19 @@ export default {
     provider: null,
     missingSlotsInARow: false
   }),
+  computed: {
+    filteredProviders: function () {
+      let filteredProviders = []
+
+      this.$store.state.data.service.providers.forEach((provider) => {
+        if (this.shouldShowProvider(provider)) {
+          filteredProviders.push(provider)
+        }
+      })
+
+      return filteredProviders
+    }
+  },
   methods: {
     formatDay: function (date) {
       return moment(date).locale('de').format('dddd, DD.MM.YYYY')
