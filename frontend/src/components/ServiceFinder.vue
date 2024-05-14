9c8cb5c52888418b30bbf14041264006ecebf99d
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2024-05-14 11:36:35 +0200

    feat(ZMS-2357): show subservice if exist

--- frontend/src/components/ServiceFinder.vue
+++ frontend/src/components/ServiceFinder.vue
@@ -85,7 +85,7 @@
           <h3 tabindex="0" v-if="$store.state.data.service.subServices.length">{{ $t('oftenBookedTogether') }}</h3>
 
           <template v-for="(subService) in $store.state.data.service.subServices">
-            <v-list-item :key="subService.id + ' ' + appointmentCountTriggered">
+            <v-list-item :key="subService.id + ' ' + appointmentCountTriggered" v-if="getServiceName(subService.id)">
               <v-card-actions>
                 <v-btn ref="buttonDown" class="appointment-count-button button-down"
                   :aria-label="`Anzahl der Dienstleistung verringern auf ` + (appointmentCounts[subService.id] - 1)"
@@ -227,7 +227,9 @@ export default {
       this.$emit('changed')
     },
     getServiceName(serviceId) {
-      return this.$store.state.servicesById[serviceId].name
+      return typeof this.$store.state.servicesById[serviceId] != 'undefined'
+          ? this.$store.state.servicesById[serviceId].name
+          : null
     },
     suggest(event) {
       if (!event.target.value) {
