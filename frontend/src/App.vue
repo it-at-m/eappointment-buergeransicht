df528af14faf832a9b3bc6c2f1b9f199c026ddf4
Thomas Fink <thomasafink@MacBook-Pro-von-Thomas.local>
2024-06-17 15:28:45 +0200

    fix not found

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,8 +6,8 @@
     <link :href="`${linkBaseUrl}css/style.css`" rel="stylesheet">
     <link :href="`${linkBaseUrl}css/patternlab.css`" rel="stylesheet">
     <link :href="`${linkBaseUrl}css/friendlycaptcha.css`" rel="stylesheet">
-    <AppointmentForm v-if="stylesLoaded && !error"/>
-    <NotFound v-if="error === 'not-found'" />
+    <AppointmentForm v-if="stylesLoaded && !errorNotFound"/>
+    <NotFound v-if="stylesLoaded && errorNotFound" />
   </v-app>
 </template>
 
