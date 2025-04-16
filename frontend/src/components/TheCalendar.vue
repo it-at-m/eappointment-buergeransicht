33fc92c09afd44087d6c01e27df0a63c764e7e24
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2025-04-16 18:24:08 +0200

    feat(ZMSKVR-309): logs

--- frontend/src/components/TheCalendar.vue
+++ frontend/src/components/TheCalendar.vue
@@ -3,7 +3,6 @@
     <v-container>
       <v-row>
         <v-col class="col-sm-12 col-lg-12 p-0">
-          {{ selectedServiceIds() }}-
           <v-tabs v-if="$store.state.data.service && $store.state.data.service.providers.length > 0" color="primary"
             show-arrows="mobile" id="location-tabs" ref="locationTabs"
             :key="$store.state.data.selectedProvider + $store.state.data.selectedServices + selectedProviderIndex === -1"
