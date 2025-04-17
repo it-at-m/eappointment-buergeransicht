d99c1753b4ea49cd43bbda75a6d4784992f06ce9
Igor Manjencic <ext.dl.manjencic@muenchen.de>
2025-04-17 12:31:37 +0200

    feat(ZMSKVR-309): clearing

--- frontend/src/components/TheCalendar.vue
+++ frontend/src/components/TheCalendar.vue
@@ -3,13 +3,12 @@
     <v-container>
       <v-row>
         <v-col class="col-sm-12 col-lg-12 p-0">
-          {{ selectedServiceIds() }}
           <v-tabs v-if="$store.state.data.service && $store.state.data.service.providers.length > 0" color="primary"
             show-arrows="mobile" id="location-tabs" ref="locationTabs"
             :key="$store.state.data.selectedProvider + $store.state.data.selectedServices + selectedProviderIndex === -1"
             v-model="selectedProviderIndex">
             <v-tab v-for="provider in filteredProviders()" :key="provider.id" @change="showForProvider(provider)">
-              {{ provider.name }}-{{ provider.disabledByServices }}
+              {{ provider.name }}
             </v-tab>
           </v-tabs>
         </v-col>
