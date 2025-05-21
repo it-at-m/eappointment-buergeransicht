b82356ce28181e5ad53442c4871231016b4e8478
Fabian Weiß <98973281+Fabinatix97@users.noreply.github.com>
2025-05-21 14:09:29 +0200

    Revert "Revert "feat(ZMSKVR-363): additional customtextfield" (#171)" (#173)
    This reverts commit 2015dd08328d0a068e6b0b6f69df5eb5336540b3.

--- frontend/__tests__/components/CustomerInfo.spec.js
+++ frontend/__tests__/components/CustomerInfo.spec.js
@@ -32,6 +32,8 @@ const wrapper = mount(CustomerInfo, {
         telephoneActivated: false,
         customTextfieldRequired: false,
         customTextfieldActivated: false,
+        customTextfield2Required: false,
+        customTextfield2Activated: false
     }
 })
 
@@ -40,6 +42,7 @@ describe('Customer info', () => {
     const emailInput = wrapper.find('#customer-email')
     const telephoneInput = wrapper.find('#customer-telephone')
     const customTextfieldInput = wrapper.find('#customer-custom-textfield')
+    const customTextfield2Input = wrapper.find('#customer-custom-textfield2')
     const dataProtectionCheckbox = wrapper.find('#customer-data-protection')
     const submitButton = wrapper.find('#customer-submit-button') 
 
