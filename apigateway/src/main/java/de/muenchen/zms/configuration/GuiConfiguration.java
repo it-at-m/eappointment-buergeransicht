1cf2fced63aa200d9b4386635f30a324e2333512
Thomas Fink <53316058+ThomasAFink@users.noreply.github.com>
2025-08-18 14:34:21 +0200

    next into main (#213)
    * fix(ZMSKVR-402): fix telephoneError and make InfoMessage dynamic (#201)
    
    * fix(deps): update dependency org.apache.commons:commons-lang3 to v3.18.0 [security] (#205)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * fix(deps): update dependency axios to ^0.30.0 [security] (#191)
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    
    * chore(ZMSKVR-773): change routing to buergeransicht1 (#212)
    
    ---------
    
    Co-authored-by: Fabian Weiß <98973281+Fabinatix97@users.noreply.github.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- apigateway/src/main/java/de/muenchen/zms/configuration/GuiConfiguration.java
+++ apigateway/src/main/java/de/muenchen/zms/configuration/GuiConfiguration.java
@@ -23,16 +23,16 @@ public class GuiConfiguration {
     public RouterFunction<ServerResponse> indexRouter(@Value("classpath:/static/index.html") final Resource indexHtml) {
         log.debug("Location of gui entry point: {}", indexHtml);
 
-        // Serve index.html at /buergeransicht
-        RouterFunction<ServerResponse> indexRoute = route(GET("/buergeransicht"),
+        // Serve index.html at /buergeransicht1
+        RouterFunction<ServerResponse> indexRoute = route(GET("/buergeransicht1"),
                 request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(indexHtml));
 
-        // Serve index.html at /buergeransicht/
-        RouterFunction<ServerResponse> indexRouteWithSlash = route(GET("/buergeransicht/"),
+        // Serve index.html at /buergeransicht1/
+        RouterFunction<ServerResponse> indexRouteWithSlash = route(GET("/buergeransicht1/"),
                 request -> ok().contentType(MediaType.TEXT_HTML).bodyValue(indexHtml));
 
-        // Serve static files from /buergeransicht/**
-        RouterFunction<ServerResponse> staticResourceRoute = resources("/buergeransicht/**",
+        // Serve static files from /buergeransicht1/**
+        RouterFunction<ServerResponse> staticResourceRoute = resources("/buergeransicht1/**",
                 new ClassPathResource("static/"));
 
         // Combine the routes
